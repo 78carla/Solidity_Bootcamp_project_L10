d7233ca419e964b5541be42303256413dce2c710
78carla <78c.carla@gmail.com>
2023-03-01 19:56:00 +0100

    Initial commit

---
+++ contracts/TokenSale.sol
@@ -0,0 +1,21 @@
+// contracts/MyERC20.sol
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./MyERC20.sol";
+interface IMyToken {
+    function mint(address to, uint256 amount) external;
+}
+contract TokenSale {
+    
+    uint256 public ratio;
+    IMyToken public tokenAddress;
+    constructor (uint256 _ratio, address _tokenAddress)  {
+        ratio = _ratio; 
+        tokenAddress = IMyToken(_tokenAddress);
+    }  
+
+    function buyTokens() public payable {
+        tokenAddress.mint(msg.sender, msg.value * ratio);
+    } 
+}
